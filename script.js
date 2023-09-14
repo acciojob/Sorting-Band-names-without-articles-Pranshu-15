0ee80d120ea5faff502b4ba064710712ea45ae51
Pranshu Agrawal <101993746+Pranshu-15@users.noreply.github.com>
2023-09-15 03:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'Led Zeppelin', 'The Beatles', 'Nirvana'];
+
+// Remove articles from band names and sort them
+bandNames = bandNames.map(name => {
+  // Remove articles 'a', 'an', 'the' (case-insensitive)
+  return name.replace(/^(a|an|the)\s+/i, '');
+}).sort();
+
+// Get the ul element with id 'band'
+const ulElement = document.getElementById('band');
+
+// Iterate through sorted band names and create li elements
+bandNames.forEach(name => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
